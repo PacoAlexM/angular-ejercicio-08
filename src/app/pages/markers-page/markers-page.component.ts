11a4c9e66912e9098e5f75015a6d1b21c14bb4e1
PacoAlexM <pako_ale_martell@hotmail.com>
2025-09-08 22:51:35 -0600

    Primer commit

---
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'markers-page',
+    imports: [],
+    templateUrl: './markers-page.component.html',
+})
+export class MarkersPageComponent {}
