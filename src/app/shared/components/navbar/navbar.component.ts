7281e2c5079832a010a2d9613c8f2f946bcf4c19
PacoAlexM <pako_ale_martell@hotmail.com>
2025-09-10 23:22:31 -0600

    Configuraciones de variables de entorno

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
     selector: 'app-navbar',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    router = inject(Router);
+
+    routes = routes.map(route => ({
+        path: route.path,
+        title: `${route.title ?? 'Mapas en Angular'}`,
+    })).filter(route => route.path !== '**');
+
+    // pageTitle$ = this.router.events.pipe(
+    //     filter(event => event instanceof NavigationEnd),
+    //     // tap(event => console.log(event)),
+    //     map(event => event.url),
+    //     map(route => routes.find(url => `/${url.path}` === route)?.title ?? '')
+    // );
+
+    pageTitle = toSignal(this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(event => event.url),
+        map(route => routes.find(url => `/${url.path}` === route)?.title ?? '')
+    ));
+}
